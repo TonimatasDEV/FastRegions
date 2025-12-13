3a056ebea867cd9d30749775fbd95e1697144802
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-12-13 17:09:18 +0100

    chore: Add GSON initializer

--- common/src/main/java/dev/tonimatas/fastregions/FastRegions.java
+++ common/src/main/java/dev/tonimatas/fastregions/FastRegions.java
@@ -1,5 +1,7 @@
 package dev.tonimatas.fastregions;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dev.tonimatas.fastregions.platform.Services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory;
 public class FastRegions {
     public static final String MOD_ID = "fastregions";
     public static final Logger LOGGER = LoggerFactory.getLogger("Fast Regions");
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     public static void init() {
         String platform = Services.PLATFORM.getPlatformName();
