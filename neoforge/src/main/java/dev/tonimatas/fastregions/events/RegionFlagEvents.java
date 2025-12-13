1991b5853d14f5a8768a364be6829cfbc890fc00
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-12-13 18:19:55 +0100

    chore: Flags have the highest priority

--- neoforge/src/main/java/dev/tonimatas/fastregions/events/RegionFlagEvents.java
+++ neoforge/src/main/java/dev/tonimatas/fastregions/events/RegionFlagEvents.java
@@ -3,13 +3,14 @@ package dev.tonimatas.fastregions.events;
 import dev.tonimatas.fastregions.FastRegions;
 import dev.tonimatas.fastregions.region.RegionEvents;
 import dev.tonimatas.fastregions.region.RegionFlag;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
 @EventBusSubscriber(modid = FastRegions.MOD_ID)
 public class RegionFlagEvents {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
         event.setCanceled(RegionEvents.cancelBlockEvent(event.getPlayer(), event.getPlayer().level(), event.getPos(), RegionFlag.BLOCK_BREAK));
     }
