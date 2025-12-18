e95319f12e29622a453cf14423a440fded76a2e4
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-12-18 19:45:44 +0100

    chore: Register commands in fabric

--- fabric/src/main/java/dev/tonimatas/fastregions/FastRegionsFabric.java
+++ fabric/src/main/java/dev/tonimatas/fastregions/FastRegionsFabric.java
@@ -1,11 +1,17 @@
 package dev.tonimatas.fastregions;
 
+import dev.tonimatas.fastregions.commands.RegionCommand;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class FastRegionsFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
         FastRegions.init();
+
+        CommandRegistrationCallback.EVENT.register((commandDispatcher, commandBuildContext, commandSelection) ->  {
+            new RegionCommand(commandDispatcher);
+        });
     }
 }
