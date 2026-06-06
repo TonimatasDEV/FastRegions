336bb7b2f5223b460c1cd130ce53cd985548a809
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-06-06 18:21:55 +0200

    feat: Load regions on Fabric

--- fabric/src/main/java/dev/tonimatas/fastregions/FastRegionsFabric.java
+++ fabric/src/main/java/dev/tonimatas/fastregions/FastRegionsFabric.java
@@ -1,8 +1,10 @@
 package dev.tonimatas.fastregions;
 
 import dev.tonimatas.fastregions.commands.RegionCommand;
+import dev.tonimatas.fastregions.region.RegionManager;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class FastRegionsFabric implements ModInitializer {
 
@@ -13,5 +15,7 @@ public class FastRegionsFabric implements ModInitializer {
         CommandRegistrationCallback.EVENT.register((commandDispatcher, commandBuildContext, commandSelection) ->  {
             new RegionCommand(commandDispatcher);
         });
+
+        ServerLifecycleEvents.SERVER_STARTED.register(RegionManager::loadRegions);
     }
 }
